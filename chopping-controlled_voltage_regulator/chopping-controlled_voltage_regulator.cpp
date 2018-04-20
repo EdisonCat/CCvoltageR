d305b39b0c05ce727800e6e5194578e9d840e18c
EdisonGuo <edisonguocat@gmail.com>
2018-04-21 00:33:19 +0800

    first push still under construction

---
+++ chopping-controlled_voltage_regulator/chopping-controlled_voltage_regulator.cpp
@@ -0,0 +1,21 @@
+#include "chopping-controlled_voltage_regulator.h"
+voltage voltageNow;
+void setup(){
+	pinMode(,INPUT);//voltage input pin
+	pinMode(,INPUT);//button input pin
+	pinMode(,INPUT);//start flag input pin
+	pinMode(,OUTPUT);//lcd output
+	pinMode(,OUTPUT);//switch1 output
+	pinMode(,OUTPUT);//switch2 output
+}
+void loop(){
+
+	voltageNow.checkVoltage();
+
+	
+
+
+	
+
+
+}
---
+++ chopping-controlled_voltage_regulator/chopping-controlled_voltage_regulator.h
@@ -0,0 +1,62 @@
+#ifndef CHOPPING-CONTROLLED_VOLTAGE_REGULATOR_H
+#define CHOPPING-CONTROLLED_VOLTAGE_REGULATOR_H
+#include "lcd.h"
+#include "Arduino.h"
+
+
+
+class voltage{
+public:
+
+	bool change_flag=false;
+	int time=200;
+	float voltage_set;
+	float voltage_current;
+
+	voltage(){
+		voltage_set=4.5;
+		voltage_current=map(analogRead(),0,255,0,5);
+		printVoltage();
+	}
+	int checkVoltage(){
+		if(voltage_current>voltage_set){
+			return time--;
+		}
+		else if(voltage_current<voltage_set){
+			return time++;
+		}
+		else
+			return time;
+
+	}
+	void switchOn(){
+		digitalWrite(,HIGH);
+		digitalWrite(,LOW);
+		delay(time);
+
+	}
+	
+	void changeFlag(){
+		switch(change_flag){
+			case(true):change_flag=false;break;
+			default:change_flag=true;
+		}
+	}
+
+	void printVoltage(){
+		lcd.print("current voltage:"voltage_current);
+		lcd.print("target voltage:"voltage_set);
+	}
+
+	void checkStatus(){
+		if(analogRead()){
+
+		}
+	}
+}
+
+
+
+
+
+#endif
